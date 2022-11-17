fd60b487884022f807369b8603d0fe226ee1aae7
Christina Bukas <ga27loj@mytum.de>
2022-11-17 16:27:27 +0100

    added pretrained cellpose for inference

--- al_framework.py
+++ al_framework.py
@@ -1,14 +1,22 @@
 import sys
 import os
 from pathlib import Path
+from typing import List
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QMainWindow, QFileSystemModel, QListView, QHBoxLayout, QFileIconProvider, QLabel, QFileDialog, QLineEdit, QTreeView
-from PyQt5.QtCore import QDir, QSize
+from PyQt5.QtCore import QSize
 from PyQt5.QtGui import QPixmap, QIcon
 import napari
 from skimage.io import imread, imsave
+from cellpose import models, utils
+import torch 
+from skimage.transform import resize
+import numpy as np
+from skimage.morphology import dilation, disk
+
+from matplotlib import pyplot as plt
 
 ICON_SIZE = QSize(128,128)
-accepted_types = (".jpg",".tiff",".png")
+accepted_types = (".jpg",".tiff",".png", ".tif")
 
 def changeWindow(w1, w2):
     w1.hide()
@@ -69,11 +77,28 @@ class NapariWindow(QWidget):
         self.setLayout(layout)
         self.show()
 
+    def _get_layer_names(self, layer_type: napari.layers.Layer = napari.layers.Labels) -> List[str]:
+        """
+        Get list of layer names of a given layer type.
+        """
+        layer_names = [
+            layer.name
+            for layer in self.viewer.layers
+            if type(layer) == layer_type
+        ]
+        if layer_names:
+            return [] + layer_names
+        else:
+            return []
+
     def on_add_button_clicked(self):
-        seg = self.viewer.layers['Labels'].data
+        label_names = self._get_layer_names()
+        seg = self.viewer.layers[label_names[0]].data
         os.replace(os.path.join(self.eval_data_path, self.img_filename), os.path.join(self.train_data_path, self.img_filename))
         seg_name = Path(self.img_filename).stem+'_seg'+Path(self.img_filename).suffix
         imsave(os.path.join(self.train_data_path, seg_name),seg)
+        if os.path.exists(os.path.join(self.eval_data_path, seg_name)): 
+            os.remove(os.path.join(self.eval_data_path, seg_name))
         self.close()
 
     '''
@@ -92,11 +117,16 @@ class MainWindow(QWidget):
 
     def main_window(self):
         self.setWindowTitle(self.title)
-        self.resize(1000, 1500)
+        #self.resize(1000, 1500)
         self.main_layout = QVBoxLayout()  
         self.top_layout = QHBoxLayout()
         self.bottom_layout = QHBoxLayout()
 
+        self.eval_dir_layout = QVBoxLayout() 
+        self.eval_dir_layout.setContentsMargins(0,0,0,0)
+        self.label_eval = QLabel(self)
+        self.label_eval.setText("Uncurated dataset")
+        self.eval_dir_layout.addWidget(self.label_eval)
         # add eval dir list
         model_eval = QFileSystemModel()
         model_eval.setIconProvider(IconProvider())
@@ -104,12 +134,18 @@ class MainWindow(QWidget):
         self.list_view_eval.setModel(model_eval)
         for i in range(1,4):
             self.list_view_eval.hideColumn(i)
-        self.list_view_eval.setFixedSize(600, 600)
+        #self.list_view_eval.setFixedSize(600, 600)
         self.list_view_eval.setRootIndex(model_eval.setRootPath(self.eval_data_path)) 
         self.list_view_eval.clicked.connect(self.item_eval_selected)
         self.cur_selected_img = None
-        self.top_layout.addWidget(self.list_view_eval)
+        self.eval_dir_layout.addWidget(self.list_view_eval)
+        self.top_layout.addLayout(self.eval_dir_layout)
 
+        self.train_dir_layout = QVBoxLayout() 
+        self.train_dir_layout.setContentsMargins(0,0,0,0)
+        self.label_train = QLabel(self)
+        self.label_train.setText("Curated dataset")
+        self.train_dir_layout.addWidget(self.label_train)
         # add train dir list
         model_train = QFileSystemModel()
         #self.list_view = QListView(self)
@@ -118,15 +154,16 @@ class MainWindow(QWidget):
         self.list_view_train.setModel(model_train)
         for i in range(1,4):
             self.list_view_train.hideColumn(i)
-        self.list_view_train.setFixedSize(600, 600)
+        #self.list_view_train.setFixedSize(600, 600)
         self.list_view_train.setRootIndex(model_train.setRootPath(self.train_data_path)) 
         self.list_view_train.clicked.connect(self.item_train_selected)
-        self.top_layout.addWidget(self.list_view_train)
+        self.train_dir_layout.addWidget(self.list_view_train)
+        self.top_layout.addLayout(self.train_dir_layout)
 
         self.main_layout.addLayout(self.top_layout)
         
         # add buttons
-        self.launch_nap_button = QPushButton("View and annotate image", self)
+        self.launch_nap_button = QPushButton("View image and fix label", self)
         self.launch_nap_button.clicked.connect(self.launch_napari_window)  # add selected image    
         self.bottom_layout.addWidget(self.launch_nap_button)
         
@@ -159,7 +196,35 @@ class MainWindow(QWidget):
         pass
 
     def run_inference(self):
-        pass
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        if device=="cuda":
+            model = models.Cellpose(gpu=True, model_type="cyto")
+        else:
+            model = models.Cellpose(gpu=False, model_type="cyto")
+        list_files = [file for file in os.listdir(self.eval_data_path) if file.endswith('.tif')]
+        for img_filename in list_files:
+            if '_seg' in img_filename:  
+                continue
+                #extend to check the prefix also matches an existing image
+                #seg_name = Path(self.img_filename).stem+'_seg'+Path(self.img_filename).suffix
+            else:
+                img = imread(os.path.join(self.eval_data_path, img_filename))
+                orig_size = img.shape
+                img = resize(img, (384, 512))
+                mask, _, _, _ = model.eval(img)
+                seg_name = Path(img_filename).stem+'_seg'+Path(img_filename).suffix
+                mask = resize(mask, orig_size, order=0)
+                outlines = utils.masks_to_outlines(mask) #[True, False] outputs
+                #outlines = dilation(outlines, disk(4))
+                new_mask = mask.copy()
+                
+                #mask[mask!=0] = 125
+                imsave('temp.tif', np.logical_and(mask, outlines))
+                new_mask[mask!=0] = 2
+                new_mask[outlines==True] = 1
+                imsave(os.path.join(self.eval_data_path, seg_name), new_mask)
+                #imsave(os.path.join(self.eval_data_path, seg_name), mask)
+
 
 class WelcomeWindow(QWidget):
     def __init__(self):
