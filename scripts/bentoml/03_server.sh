41a40554f07361a4e2894222105a72d5a283926e
gerome.vivar <gerome.vivar@helmholtz-muenchen.de>
2023-01-10 17:31:07 +0100

    refactor: rename bentoml bash script

--- scripts/bentoml/03_server.sh
+++ scripts/bentoml/03_server.sh
@@ -1,6 +1,8 @@
 # Development mode
+/home/ubuntu/active-learning-platform/src/active_learning_platform/server
 bentoml serve service:svc --reload --port=7010
 
 # Non-dev model with ssl certs
-bentoml serve service:svc --reload --port=7010 \
+/home/ubuntu/active-learning-platform/src/active_learning_platform/server
+bentoml serve service:svc --port=7010 \
 --ssl-certfile ~/.ssl/certs/nginx-selfsigned.crt --ssl-keyfile ~/.ssl/private/nginx-selfsigned.key
