ecb7217173f8e9d6de9321490ddc152b8a411c1e
Nick A. Del Grosso <delgrosso.nick@gmail.com>
2023-03-11 16:36:34 +0100

    updated pyproject with package path and console script, pass bento working directory to installation folder to serve command

--- src/server/dcp_server/main.py
+++ src/server/dcp_server/main.py
@@ -1,9 +1,20 @@
 import subprocess
+from os import path
+
 
 def main(args=None):
     '''entry point to bentoml
     '''
-    subprocess.run(["bentoml","serve","service:svc","--reload","--port=7010"])
+    local_path = path.join(__file__, '..')
+    subprocess.run([
+        "bentoml",
+        "serve", 
+        '--working-dir', 
+        local_path,
+        "service:svc",
+        "--reload",
+        "--port=7010",
+    ])
     
 if __name__ == "__main__":
     main()
