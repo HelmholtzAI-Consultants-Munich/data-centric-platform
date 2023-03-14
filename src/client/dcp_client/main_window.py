e4e9570d3eee48dc6b04bc534de90326caecfcf1
Mahyar Valizadeh <mahyar.valizadeh@helmholtz-muenchen.de>
2023-03-14 16:58:17 +0100

    refactored the main window

--- src/client/dcp_client/main_window.py
+++ src/client/dcp_client/main_window.py
@@ -7,6 +7,38 @@ import settings
 from utils import IconProvider, create_warning_box
 from napari_window import NapariWindow
 
+class Application:
+    def __init__(self, eval_data_path, train_data_path, inprogr_data_path) -> None:
+    
+        self.eval_data_path = eval_data_path
+        self.train_data_path = train_data_path
+        self.inprogr_data_path = inprogr_data_path
+        self.client = Client.from_url("http://0.0.0.0:7010") # have the url of the bentoml service here
+        self.cur_selected_img = None
+    
+    # run train
+    async def _run_train(self):
+        response = await self.client.async_retrain(self.train_data_path)
+        return response
+    
+    def run_train(self):
+        return asyncio.run(self._run_train())
+    
+    async def _run_inference(self):
+        response = await self.app.client.async_segment_image(self.app.eval_data_path)
+        return response
+    def run_inference(self):
+        list_of_files_not_suported = asyncio.run(self._run_inference())
+        list_of_files_not_suported = list(list_of_files_not_suported)
+        if len(list_of_files_not_suported) > 0:
+            message_text = "Image types not supported. Only 2D and 3D image shapes currently supported. 3D stacks must be of type grayscale. \
+            Currently supported image file formats are: ", settings.accepted_types, "The files that were not supported are: " + ", ".join(list_of_files_not_suported)
+            message_title = "Warning"
+        else:
+            message_text = "Success! Masks generated for all images"
+            message_title="Success"
+        return message_text, message_title
+
 class MainWindow(QWidget):
     '''Main Window Widget object.
     Opens the main window of the app where selected images in both directories are listed. 
@@ -19,14 +51,10 @@ class MainWindow(QWidget):
 
     def __init__(self, eval_data_path, train_data_path, inprogr_data_path):
         super().__init__()
-
+        self.app = Application(eval_data_path, train_data_path, inprogr_data_path)
         self.title = "Data Overview"
-        self.eval_data_path = eval_data_path
-        self.train_data_path = train_data_path
-        self.inprogr_data_path = inprogr_data_path
-
-        self.client = Client.from_url("http://0.0.0.0:7010") # have the url of the bentoml service here
         self.main_window()
+        
 
 
     def main_window(self):
@@ -51,15 +79,15 @@ class MainWindow(QWidget):
         for i in range(1,4):
             self.list_view_eval.hideColumn(i)
         #self.list_view_eval.setFixedSize(600, 600)
-        self.list_view_eval.setRootIndex(model_eval.setRootPath(self.eval_data_path)) 
-        self.list_view_eval.clicked.connect(self.item_eval_selected)
-        self.cur_selected_img = None
+        self.list_view_eval.setRootIndex(model_eval.setRootPath(self.app.eval_data_path)) 
+        self.list_view_eval.clicked.connect(self.on_item_eval_selected)
+        
         self.eval_dir_layout.addWidget(self.list_view_eval)
         self.uncurated_layout.addLayout(self.eval_dir_layout)
 
         # add buttons
         self.inference_button = QPushButton("Generate Labels", self)
-        self.inference_button.clicked.connect(self.on_run_inference_button_clicked)  # add selected image    
+        self.inference_button.clicked.connect(self.on_inference_button_clicked)  # add selected image    
         self.uncurated_layout.addWidget(self.inference_button, alignment=Qt.AlignCenter)
 
         self.main_layout.addLayout(self.uncurated_layout)
@@ -79,13 +107,13 @@ class MainWindow(QWidget):
         for i in range(1,4):
             self.list_view_inprogr.hideColumn(i)
         #self.list_view_inprogr.setFixedSize(600, 600)
-        self.list_view_inprogr.setRootIndex(model_inprogr.setRootPath(self.inprogr_data_path)) 
-        self.list_view_inprogr.clicked.connect(self.item_inprogr_selected)
+        self.list_view_inprogr.setRootIndex(model_inprogr.setRootPath(self.app.inprogr_data_path)) 
+        self.list_view_inprogr.clicked.connect(self.on_item_inprogr_selected)
         self.inprogr_dir_layout.addWidget(self.list_view_inprogr)
         self.inprogress_layout.addLayout(self.inprogr_dir_layout)
 
         self.launch_nap_button = QPushButton("View image and fix label", self)
-        self.launch_nap_button.clicked.connect(self.launch_napari_window)  # add selected image    
+        self.launch_nap_button.clicked.connect(self.on_launch_napari_button_clicked)  # add selected image    
         self.inprogress_layout.addWidget(self.launch_nap_button, alignment=Qt.AlignCenter)
 
         self.main_layout.addLayout(self.inprogress_layout)
@@ -105,8 +133,8 @@ class MainWindow(QWidget):
         for i in range(1,4):
             self.list_view_train.hideColumn(i)
         #self.list_view_train.setFixedSize(600, 600)
-        self.list_view_train.setRootIndex(model_train.setRootPath(self.train_data_path)) 
-        self.list_view_train.clicked.connect(self.item_train_selected)
+        self.list_view_train.setRootIndex(model_train.setRootPath(self.app.train_data_path)) 
+        self.list_view_train.clicked.connect(self.on_train_item_selected)
         self.train_dir_layout.addWidget(self.list_view_train)
         self.curated_layout.addLayout(self.train_dir_layout)
         
@@ -119,48 +147,33 @@ class MainWindow(QWidget):
         self.setLayout(self.main_layout)
         self.show()
 
-    def launch_napari_window(self):   
+    def on_train_item_selected(self, item):
+        self.app.cur_selected_img = item.data()
+
+    def on_item_eval_selected(self, item):
+        self.app.cur_selected_img = item.data()
+    
+    def on_item_inprogr_selected(self, item):
+        self.app.cur_selected_img = item.data()
+
+    def on_launch_napari_button_clicked(self):   
         ''' 
         Launches the napari window after the image is selected.
         '''
-
-        if not self.cur_selected_img or '_seg.tiff' in self.cur_selected_img:
+        if not self.app.cur_selected_img or '_seg.tiff' in self.app.cur_selected_img:
             message_text = "Please first select an image you wish to visualise. The selected image must be an original images, not a mask."
-            create_warning_box(message_text, message_title="Warning")
-        else:
-            self.nap_win = NapariWindow(img_filename=self.cur_selected_img, 
-                                        eval_data_path=self.eval_data_path, 
-                                        train_data_path=self.train_data_path,
-                                        inprogr_data_path=self.inprogr_data_path)
-            self.nap_win.show()
-
-    def item_eval_selected(self, item):
-        self.cur_selected_img = item.data()
-    
-    def item_train_selected(self, item):
-        self.cur_selected_img = item.data()
-
-    def item_inprogr_selected(self, item):
-        self.cur_selected_img = item.data()
-
-    async def _run_train(self):
-        response = await self.client.async_retrain(self.train_data_path)
-        return response
-
-    def on_train_button_clicked(self):
-        message_text = asyncio.run(self._run_train())
-        create_warning_box(message_text)
-
-    async def _run_inference(self):
-        response = await self.client.async_segment_image(self.eval_data_path)
-        return response
-
-    def on_run_inference_button_clicked(self):
-        list_of_files_not_suported = asyncio.run(self._run_inference())
-        list_of_files_not_suported = list(list_of_files_not_suported)
-        if len(list_of_files_not_suported) > 0:
-            message_text = "Image types not supported. Only 2D and 3D image shapes currently supported. 3D stacks must be of type grayscale. \
-            Currently supported image file formats are: ", settings.accepted_types, "The files that were not supported are: " + ", ".join(list_of_files_not_suported)
             create_warning_box(message_text)
         else:
-            create_warning_box("Success! Masks generated for all images", message_title="Success")
+            self.nap_win = NapariWindow(img_filename=self.app.cur_selected_img, 
+                                        eval_data_path=self.app.eval_data_path, 
+                                        train_data_path=self.app.train_data_path,
+                                        inprogr_data_path=self.app.inprogr_data_path)
+            self.nap_win.show()
+
+    def on_train_button_clicked(self):
+        message_text = self.app.run_train()
+        create_warning_box(message_text)
+
+    def on_inference_button_clicked(self):
+        message_text, message_title = self.app.run_inference()
+        create_warning_box(message_text, message_title)
