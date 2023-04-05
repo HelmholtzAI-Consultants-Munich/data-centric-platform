49ecaf308c6533a7ed0583a96b6c70bc9ca26ef2
hpelin <helena.pelin@gmail.com>
2023-04-05 16:51:01 +0200

    empty, the models will go here

---
+++ src/server/dcp_server/models.py
@@ -0,0 +1,3 @@
+# Here, we will define the models class which will enable us to have more flexibility on the model parameters
+# Curently, we decide only on the model type when definining the bentoml runner in service.py
+
