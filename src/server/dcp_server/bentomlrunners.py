92da36853cbd0e224c1a85faf3f1f881742b4c0d
hpelin <helena.pelin@gmail.com>
2023-04-17 11:30:14 +0200

    runnable generalized

--- src/server/dcp_server/bentomlrunners.py
+++ src/server/dcp_server/bentomlrunners.py
@@ -2,24 +2,23 @@ from typing import List
 from cellpose import models
 import bentoml
 import numpy as np
+from models import CustomCellposeModel
 
 
-class CellposeRunnable(bentoml.Runnable):
-    SUPPORTED_RESOURCES = ("cpu",)
+class CustomRunnable(bentoml.Runnable):
+    SUPPORTED_RESOURCES = ("cpu",) #TODO add here?
     SUPPORTS_CPU_MULTI_THREADING = False
 
-    def __init__(self, cellpose_model_type = 'cyto'):
-        #self.model = models.Cellpose(gpu=True, model_type="cyto")
-        #self.model_path = # we need to add the model path here
-        self.model = models.CellposeModel(gpu=True, model_type=cellpose_model_type)
+    def __init__(self, model):
+        self.model = model
 
     @bentoml.Runnable.method(batchable=False)
     def evaluate(self, img: np.ndarray, z_axis: int) -> np.ndarray: # flag: int
-        mask, _, _ = self.model.eval(img, z_axis=z_axis)
+        mask, _, _ = self.model.eval(img=img, z_axis=z_axis)
         return mask
 
     @bentoml.Runnable.method(batchable=False)
-    def train(self, imgs: List[np.ndarray], masks: List[np.ndarray]) -> str:
+    def train(self, imgs: List[np.ndarray], masks: List[np.ndarray], **kwargs) -> str:
         save_model_path = 'mytrainedmodel' # if we want to replace existing model here set this to self.model_path 
-        self.model.train(imgs, masks, n_epochs=2, channels=[0], save_path=save_model_path)
+        self.model.train(train_data = imgs, train_labels = masks, n_epochs=2, channels=[0], save_path=save_model_path, **kwargs)
         return save_model_path
