17e60d858523d7744a75d6e514d9bafe667e24ef
hpelin <helena.pelin@gmail.com>
2023-04-18 11:06:14 +0200

    cleaning

--- src/server/dcp_server/bentomlrunners.py
+++ src/server/dcp_server/bentomlrunners.py
@@ -8,18 +8,22 @@ class CustomRunnable(bentoml.Runnable):
     SUPPORTS_CPU_MULTI_THREADING = False
 
     def __init__(self, model, save_model_path):
+        
         self.model = model
         self.save_model_path = save_model_path
 
     @bentoml.Runnable.method(batchable=False)
     def evaluate(self, img: np.ndarray, z_axis: int) -> np.ndarray: # flag: int
+
         mask, _, _ = self.model.eval(img=img, z_axis=z_axis)
+
         return mask
 
     @bentoml.Runnable.method(batchable=False)
     def train(self, imgs: List[np.ndarray], masks: List[np.ndarray]) -> str:
+
         self.model.train(imgs, masks)
         # Save the benotml model
         bentoml.picklable_model.save_model(self.save_model_path, self.model) 
-        
+
         return self.save_model_path
