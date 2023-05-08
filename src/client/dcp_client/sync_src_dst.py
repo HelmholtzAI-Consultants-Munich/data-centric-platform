679a0d5d7507b58be8e9f1c6a20593cabda79ab4
hpelin <helena.pelin@gmail.com>
2023-05-08 16:20:08 +0200

    working server access, added fixed srv repo path

--- src/client/dcp_client/sync_src_dst.py
+++ src/client/dcp_client/sync_src_dst.py
@@ -7,8 +7,9 @@ class DataRSync(DataSync):
     Class which uses rsync bash command to sync data between client and server
     '''
     def __init__(self,
+                 user_name=None,
                  host_name=None,
-                 host_ip=None,
+                 server_repo_path='/home/ubuntu/data-centric-platform/data-sync/'
     ):
         """Constructs all the necessary attributes for the CustomRunnable.
 
@@ -17,31 +18,42 @@ class DataRSync(DataSync):
         :param host_ip: the host ip of the server - if None, then it is assumed that local machine is used for the server
         :type host_ip: str
         """  
+        self.user_name = user_name
         self.host_name = host_name
-        self.host_ip = host_ip
+        self.server_repo_path = server_repo_path
+
+        self.server = self.user_name + "@" + self.host_name + ":" + self.server_repo_path
 
     def sync(self, src, dst, path):
-        rel_path = get_relative_path(path)
-        server = self.host_name+"@"+self.host_ip+":"+rel_path+"/"
+
+        print(f'server is {self.server}')
         if src=='server':
-            src = server
+            src = self.server
             dst = path
         else:
             src = path
-            dst = server
+            dst = self.server
         
         subprocess.run(["rsync",
                         "-r" ,
                         "--delete", 
                         src, 
                         dst])
-        return rel_path
+        #return rel_path
+        return self.server_repo_path
         
 
 if __name__=="__main__":
-    ds = DataRSync(host_name="ubuntu",
-                   host_ip="134.94.88.74")
+    ds = DataRSync(user_name="ubuntu",
+                   user_name="jusuf-vm2", 
+                   server_repo_path="/home/ubuntu/data-centric-platform/data-sync/") #vm2
+    # These combinations work for me:
+    # ubuntu@jusuf-vm2:/path...
+    # jusuf-vm2:/path...
     dst = "server"
     src = "client"
-    path = "data/"
+    # dst = 'client'
+    # src = 'server'
+    #path = "data/"
+    path = "/Users/helena.pelin/Desktop/images/test/"
     ds.sync(src, dst, path)
