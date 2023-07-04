49ed753c1e30740eeda4f6e5ad2aede3eaa397a1
Christina Bukas <ga27loj@mytum.de>
2023-07-04 13:38:17 +0200

    removed commented code

--- src/client/dcp_client/main.py
+++ src/client/dcp_client/main.py
@@ -17,15 +17,10 @@ def main():
     image_storage = FilesystemImageStorage()
     ml_model = BentomlModel()
     data_sync = DataRSync()
-    '''
-    data_sync = DataRSync(user_name="ubuntu",
-                          host_name="jusuf-vm2",
-                          server_repo_path="/home/ubuntu/data-centric-platform/data-sync/")
-    '''
     welcome_app = Application(ml_model=ml_model, 
                               syncer=data_sync,
                               image_storage=image_storage,
-                              server_ip='0.0.0.0',
+                              server_ip='134.94.88.74', #'0.0.0.0',
                               server_port=7010)
     app = QApplication(sys.argv)
     window = WelcomeWindow(welcome_app)
