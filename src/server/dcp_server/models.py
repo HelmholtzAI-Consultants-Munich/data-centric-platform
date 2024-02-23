88ed260e31d35432e2082db0a2cd2148f75a8e27
Christina Bukas <ga27loj@mytum.de>
2024-02-23 16:27:37 +0100

    added documentation

--- src/server/dcp_server/models.py
+++ src/server/dcp_server/models.py
@@ -729,9 +729,10 @@ class CellposeMultichannel():
 
             for mask in masks:
                 mask_class = mask.copy()
+                # set all instances in the instance mask not corresponding to the class in question to zero
                 mask_class[0][mask_class[1]!=(i+1)] = 0
                 masks_class.append(mask_class)
-
+            
             self.cellpose_models[i].train(imgs, masks_class)
 
         self.metric = np.mean([self.cellpose_models[i].metric for i in range(self.num_of_channels)])
@@ -739,7 +740,8 @@ class CellposeMultichannel():
 
 
     def eval(self, img):
-        """Evaluate the model on the provided image.
+        """Evaluate the model on the provided image. The instance mask are computed as the union of the predicted model outputs, while the class of
+        each object is assigned based on majority voting between the models.
 
         :param img: Input image for evaluation.
         :type img:  np.ndarray[np.uint8]
@@ -750,28 +752,45 @@ class CellposeMultichannel():
         instance_masks, class_masks, model_confidences = [], [], []
 
         for i in range(self.num_of_channels):
-
+            # get the instance mask and pixel-wise cell probability mask
             instance_mask, probs, _  = self.cellpose_models[i].eval_all_outputs(img)
             confidence = probs[2]
+            # assign the appropriate class to all objects detected by this model
             class_mask = np.zeros_like(instance_mask)
             class_mask[instance_mask>0]=(i + 1)
                         
             instance_masks.append(instance_mask)
             class_masks.append(class_mask)
             model_confidences.append(confidence)
-
+        # merge the outputs of the different models using the pixel-wise cell probability mask
         merged_mask_instances, class_mask = self.merge_masks(instance_masks, class_masks, model_confidences)
+        # set all connected components to the same label in the instance mask
         instance_mask = label_mask(merged_mask_instances>0)
+        # and set the class with the most pixels to that object
         for inst_id in np.unique(instance_mask)[1:]:   
             where_inst_id = np.where(instance_mask==inst_id)
             vals, counts = np.unique(class_mask[where_inst_id], return_counts=True)
             class_mask[where_inst_id] = vals[np.argmax(counts)]
-
+        # take the final mask by stancking instance and class mask
         final_mask = np.stack((instance_mask, class_mask), axis=self.eval_config['mask_channel_axis']).astype(np.uint16)
         
         return final_mask
     
     def merge_masks(self, inst_masks, class_masks, probabilities):
+        """Merges the instance and class masks resulting from the different models using the pixel-wise cell probability. The output of the model
+        with the maximum probability is selected for each pixel.
+
+        :param inst_masks: List of predicted instance masks from each model.
+        :type inst_masks:  List[np.array]
+        :param class_masks: List of corresponding class masks from each model.
+        :type class_masks:  List[np.array]
+        :param probabilities: List of corresponding pixel-wise cell probability masks
+        :type probabilities:  List[np.array]
+        :return: A tuple containing the following elements:
+            - final_mask_inst (numpy.ndarray): A single instance mask where for each pixel the output of the model with the highest probability is selected
+            - final_mask_class (numpy.ndarray): A single class mask where for each pixel the output of the model with the highest probability is selected
+        :rtype: tuple
+        """
         # Convert lists to numpy arrays
         inst_masks = np.array(inst_masks)
         class_masks = np.array(class_masks)
